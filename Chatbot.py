166b023639fb8890b028f37e14042a1e28eba014
JRutled2 <JDDRutledge@gmail.com>
2023-10-12 14:50:32 -0400

    Changed past week acomplishments to static questions

--- Chatbot.py
+++ Chatbot.py
@@ -5,40 +5,39 @@ class Bot():
     def __init__(self, gpt_key):
         openai.api_key = gpt_key
 
+        self.team_members = ['Mike', 'John', 'Bill']
+
         self.message_log = []
 
-    def __chat(prompt):
+    def chat(self, prompt):
         self.message_log += [{'role': 'user', 'content': prompt}]
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.message_log, temperature=0)
         self.message_log += [{'role': 'assistant', 'content': completion.choices[0].message.content}]
         return completion.choices[0].message.content
 
-    def weekly_chat():
-        print('Hello, What did you acomplish in the past week.')
-        message_log = [{'role': 'system', 'content': 'You are a professor interview groups for a class, as them what they acomplished in the last week.  When they say that is all they completed, say "Done!"'},
-                    {'role': 'assistant', 'content': 'Hello, What did you acomplish in the past week?'}]
+    def test(self, ):
+        print(str(self.team_members))
 
-        # Questions about what was completed in the past week
+    def weekly_chat(self, ):
+        # Asks Each team member what they acomplished in the past week
+        print(f'Hello, What did {self.team_members[0]} acomplish in the past week.')
         userin = input('> ')
-        while userin != 'quit':
-        response = chat(userin)
-        if 'Done!' in response:
-            break
-        print(response)
-        userin = input('> ')
+        for i in self.team_members[1:]:
+            print(f'Thank you, now what did {i} acomplish in the past week.')
+            userin = input('> ')
 
-
-        message_log = [{'role': 'system', 'content': "You are asking me about what I plan to complete in the next week."},
-                        {'role': 'system', 'content': "If my statements don't fulfll the S.M.A.R.T. goals, ask me more questions that will fulfill them.  Only ask me one question at a time.  When I have fulfilled all the S.M.A.R.T. goals, say Done!"}]
+        # Asks about what they plan on completing in the upcoming week
+        message_log = [{'role': 'system', 'content': f'You are asking a group about what they plan to complete in the next week.  The team members in the group are {self.team_members}.'},
+                       {'role': 'system', 'content': "If their statements don't fulfll the S.M.A.R.T. goals, ask me more questions that will fulfill them.  Make sure each team member plans on acomplishing something.  Only ask them one question at a time.  When they have fulfilled all the S.M.A.R.T. goals, say Done!"}]
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=message_log, temperature=0)
         print(completion.choices[0].message.content)
         message_log += [{'role': 'assistant', 'content': completion.choices[0].message.content}]
         userin = input('> ')
         while userin == 'quit':
-        response = chat(userin)
-        if 'Done!' in response:
-            break
-        print(response)
-        userin = input('> ')
+            response = self.chat(userin)
+            if 'Done!' in response:
+                break
+            print(response)
+            userin = input('> ')
 
         print('Finished!')
--- Webcode.py
+++ Webcode.py
@@ -1,4 +1,6 @@
 from Chatbot import Bot
+from flask import Flask
 
 b = Bot('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')
 
+b.weekly_chat()
