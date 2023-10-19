9d83bc945364f6058456a55eb3dbfdf140d0b4a4
JRutled2 <JRutled2@mail.umw.edu>
2023-10-19 18:45:04 -0400

    More Documentation

--- Chatbot.py
+++ Chatbot.py
@@ -2,16 +2,36 @@ from flask.wrappers import Response
 import openai
 
 class Bot():    
+    """ Interview Bot Class
     
-    def __init__(self, gpt_key: str) -> None:
+    The interview bot it used to conduct interviews with students
+    on how their project is progressing
+    
+    Attributes:
+        team_members (list[str]): A list of all the team members working on the project.
+        message_log (list[dict[str]]): A list of messages between the user, system, and GPT, 
+            each element in the list is a dictionary containing the role and content.
+        chat_stage (int): Keeps track of current stage in the interview, used for the chat function.
+            chat_stage  Chat aspect         Method Called
+            ---------   -----------         -------------
+                0       Previous Week       chat_previous_week()
+                1       Previous Problems   XXX
+                2       Plans               XXX
+                3       Concerns            XXX
+    """
+    
+    def __init__(self, gpt_key: str, ) -> None:  
+        # Stores all the team members
+        self.team_members: list[str] = []
+        # Stores the message log
+        self.message_log: list[dict[str]] = []
+        # Current stage in chat function
+        self.chat_stage: int = 0
+        # Temp List of Team members used for tracking
+        self.temp_members: list[str] = self.team_members
+
         # Sets the openai key
         openai.api_key = gpt_key
-        # Stores all the team members
-        self.team_members = []
-        # Stores the message log
-        self.message_log = []
-        # Current stage in chat function
-        self.chat_stage = 0
 
     def ask_gpt(self, ) -> None:
         """ Method that Generates a New Response From the GPT Model
@@ -30,11 +50,34 @@ class Bot():
         # Adds GPT output to the message log
         self.message_log += [{'role': 'assistant', 'content': completion.choices[0].message.content}]
 
-    def chat(self, user_input: str) -> list[dict[str]]:
+    def chat(self, user_input: str, ) -> list[dict[str]]:
         """ Chat Method That the User Interacts With
         
+        This is the main function that the user calls when using the Chatbot.
+        This function calls other, more specific funtions depending on the current chat stage. 
+        
+        Args:
+            user_input (str): The input from the user
+        
+        Returns:
+            list[dict[str]]: This method returns the current chatlog, past chats and any newly added chats
         """
+        
+        # If the userinput is not empty, then it adds it onto the end.
+        # The user input will only be empty on the first time chat is called.
+        if user_input != '':
+            self.message_log += [{'role': 'user', 'content': user_input}]
+            
+        # Calls appropriate method based on chat stage
+        if self.chat_stage == 0:
+            self.chat_previous_week()
+
         return self.message_log
+    
+    def chat_previous_week(self, ) -> None:
+        pass
+    
+        
     
     # Old Chat Function
     def OLD_weekly_chat(self, ):
