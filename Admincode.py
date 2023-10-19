1b71ba537b22f1a1cc79f5479c74a2c299a024bf
JRutled2 <JDDRutledge@gmail.com>
2023-10-19 15:00:16 -0400

    Added Database

--- Admincode.py
+++ Admincode.py
@@ -1,4 +1,17 @@
 from Chatbot import Bot
+import sqlite3
 
 def create_DB():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    f = open('blueprint.sql','r').read()
+    c.executescript(f)
+    conn.commit()
+    c.close()
+    conn.close()
     pass
+
+def add_dummy_user():
+    pass
+
+create_DB()
