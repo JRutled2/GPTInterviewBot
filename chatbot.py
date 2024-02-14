919908c523d35eba25b04c1acd17bfc20a4f5004
JRutled2 <JRutled2@mail.umw.edu>
2024-02-14 15:23:26 -0500

    Removed Old Chat Function

--- chatbot.py
+++ chatbot.py
@@ -154,26 +154,3 @@ class Bot():
         
         # Adds the question about concerns to the message_log
         self.message_log += [{'role': 'assistant', 'content': f'Do you have any other comments or concerns?'}]
-
-    # Old Chat Function
-    def OLD_weekly_chat(self, ):
-        # Asks Each team member what they acomplished in the past week
-        print(f'Hello, What did {self.team_members[0]} acomplish in the past week.')
-        userin = input('> ')
-        for i in self.team_members[1:]:
-            print(f'Thank you, now what did {i} acomplish in the past week.')
-            userin = input('> ')
-
-        # Asks about what they plan on completing in the upcoming week
-        self.message_log = [{'role': 'system', 'content': "You are asking me about what I plan to complete in the next week."},
-                {'role': 'system', 'content': "If my statements don't fulfll the S.M.A.R.T. goals, ask me more questions that will fulfill them.  Only ask me one question at a time.  When I have fulfilled all the S.M.A.R.T. goals, say Done!"}]
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.message_log, temperature=0)
-        print(completion.choices[0].message.content)
-        self.message_log += [{'role': 'assistant', 'content': completion.choices[0].message.content}]
-        userin = input('> ')
-        while userin != 'quit':
-            response = self.chat(userin)
-            if 'Done!' in response:
-                break
-            print(response)
-            userin = input('> ')
